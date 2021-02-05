a94f39b68709d89d276eb7c63044c374a473eaf9
Juciano Victor <jucianovictor95@gmail.com>
2021-02-05 12:54:49 -0300

    App module, component e routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  pathMatch: 'full', loadChildren: () => import('./pages/profissional/profissional.module').then(m => m.ProfissionalModule) },
+  { path: 'estabelecimento', loadChildren: () => import('./pages/estabelecimento/estabelecimento.module').then(m => m.EstabelecimentoModule) }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
